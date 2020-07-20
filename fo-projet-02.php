c96831f18db850b2e3d4274a78933475c7444219
Fleury VUADIAMBO <matanu.fv@gmail.com>
2020-07-20 14:45:24 +0200

    modification pop up

--- fo-projet-02.php
+++ fo-projet-02.php
@@ -24,6 +24,7 @@
         .overlay {position: fixed; top: 0; bottom: 0; left: 0; right: 0; background: rgba(0, 0, 0, 0.7); transition: opacity 500ms; visibility: hidden; opacity: 0; }
         .overlay:target {visibility: visible; opacity: 1; }
         .popup {margin: 70px auto; padding: 20px; background: #fff; border-radius: 5px; width: 50%; position: relative; transition: all 5s ease-in-out; }
+        .close {position: absolute; top: 5%; right: 3%; font-size: xx-large; }
         .valid {text-align: center; margin-top: 20px; }
 
         
@@ -174,8 +175,8 @@
         <div class="popup">
             <h2 class="text-center m-4">Projet : <span>Développement IP</span> </h2>
             <a class="close" href="#">&times;</a>
-            <div class="col-sm-8 mx-auto">
-                <textarea class="form-control" id="liste_projets" rows="25">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis doloremque voluptate similique tempora eius alias, vitae reprehenderit est, molestiae illum vero consequatur illo quis blanditiis ab nemo consectetur delectus nisi!</textarea>
+            <div class="col-lg mx-auto">
+                <textarea class="form-control" id="liste_projets" rows="15">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis doloremque voluptate similique tempora eius alias, vitae reprehenderit est, molestiae illum vero consequatur illo quis blanditiis ab nemo consectetur delectus nisi!</textarea>
             </div> 
             <div class="valid">
                 <input type="button" class="btn btn-success" value="Valider">
