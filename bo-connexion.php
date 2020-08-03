0c5bc775f8fca45d84cc2fb47e52a7cc0b410c77
yanny78@outlook.fr <yanny78@outlook.fr>
2020-08-03 12:25:18 +0200

    correction de l'hortographe changment de couleurs des boutton

--- bo-connexion.php
+++ bo-connexion.php
@@ -24,11 +24,6 @@
       include ('bo-header_co.php');
       ?>
 
-      <?php 
-	include ("bo-menu-horizontal.php"); 
-	?>
-
-
        
 
        <body id="particles-js"></body>
@@ -47,7 +42,7 @@
         <input type="text" name="email" placeholder="Email" autocomplete="off">
         <i class="typcn typcn-eye" id="eye"></i>
         <input type="password" name="Nom" placeholder="Mot de Passe" id="pwd" autocomplete="off">
-        <a href="#" class="forgetpass" style="color:white;">Mot De Passe,oublier?</a>
+        <a href="#" class="forgetpass" style="color:white;">Mot De Passe oublier?</a>
         <a href="bo-gestion_societe.php"><input type="submit" value="Connexion" class="btn1"></a>
       </form>
   </div> 
@@ -71,180 +66,7 @@
       </script>
 
     <script>
-      $('#login-button').click(function(){
-  $('#login-button').fadeOut("slow",function(){
-    $("#container").fadeIn();
-    TweenMax.from("#container", .4, { scale: 0, ease:Sine.easeInOut});
-    TweenMax.to("#container", .4, { scale: 1, ease:Sine.easeInOut});
-  });
-});
-
-$(".close-btn").click(function(){
-  TweenMax.from("#container", .4, { scale: 1, ease:Sine.easeInOut});
-  TweenMax.to("#container", .4, { left:"0px", scale: 0, ease:Sine.easeInOut});
-  $("#container, #forgotten-container").fadeOut(800, function(){
-    $("#login-button").fadeIn(800);
-  });
-});
-
-var pwd = document.getElementById('pwd');
-var eye = document.getElementById('eye');
-
-eye.addEventListener('click',togglePass);
-
-function togglePass(){
-
-   eye.classList.toggle('active');
-
-   (pwd.type == 'password') ? pwd.type = 'text' : pwd.type = 'password';
-}
-
-// Form Validation
-
-function checkStuff() {
-  var email = document.form1.email;
-  var password = document.form1.password;
-  var msg = document.getElementById('msg');
-  
-  if (email.value == "") {
-    msg.style.display = 'block';
-    msg.innerHTML = "Please enter your email";
-    email.focus();
-    return false;
-  } else {
-    msg.innerHTML = "";
-  }
-  
-   if (password.value == "") {
-    msg.innerHTML = "Please enter your password";
-    password.focus();
-    return false;
-  } else {
-    msg.innerHTML = "";
-  }
-   var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (!re.test(email.value)) {
-    msg.innerHTML = "Please enter a valid email";
-    email.focus();
-    return false;
-  } else {
-    msg.innerHTML = "";
-  }
-}
-
-// ParticlesJS
-
-// ParticlesJS Config.
-particlesJS("particles-js", {
-  "particles": {
-    "number": {
-      "value": 60,
-      "density": {
-        "enable": true,
-        "value_area": 800
-      }
-    },
-    "color": {
-      "value": "#ffffff"
-    },
-    "shape": {
-      "type": "circle",
-      "stroke": {
-        "width": 0,
-        "color": "#000000"
-      },
-      "polygon": {
-        "nb_sides": 5
-      },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
-      }
-    },
-    "opacity": {
-      "value": 0.1,
-      "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 1,
-        "opacity_min": 0.1,
-        "sync": false
-      }
-    },
-    "size": {
-      "value": 6,
-      "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 40,
-        "size_min": 0.1,
-        "sync": false
-      }
-    },
-    "line_linked": {
-      "enable": true,
-      "distance": 150,
-      "color": "#ffffff",
-      "opacity": 0.1,
-      "width": 2
-    },
-    "move": {
-      "enable": true,
-      "speed": 1.5,
-      "direction": "top",
-      "random": false,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
-      }
-    }
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": false,
-        "mode": "repulse"
-      },
-      "onclick": {
-        "enable": false,
-        "mode": "push"
-      },
-      "resize": true
-    },
-    "modes": {
-      "grab": {
-        "distance": 400,
-        "line_linked": {
-          "opacity": 1
-        }
-      },
-      "bubble": {
-        "distance": 400,
-        "size": 40,
-        "duration": 2,
-        "opacity": 8,
-        "speed": 3
-      },
-      "repulse": {
-        "distance": 200,
-        "duration": 0.4
-      },
-      "push": {
-        "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
-      }
-    }
-  },
-  "retina_detect": true
-});
+     
 
 </script>
   
--- bo-menu-horizontal.php
+++ bo-menu-horizontal.php
@@ -1,14 +1,14 @@
 <ul id="menu-demo1">
 	<li> <a href="">Gestion des utilisateurs</a> 
 		<ul>
-			<li><a href="bo-gestion_utili.php">Liste des Utilisateur</a></li>
-			<li><a href="bo-form_utili.php">Ajouter Utilisateur</a></li>
+			<li><a href="bo-gestion_utili.php">Liste des utilisateurs</a></li>
+			<li><a href="bo-form_utili.php">Ajouter utilisateur</a></li>
 		</ul>
 	</li>
-	<li><a href="">Gestion des Sociétés</a>
+	<li><a href="">Gestion des sociétés</a>
 		<ul>
-			<li><a href="bo-gestion_societe.php">Liste des Sociétés</a></li>
-			<li><a href="bo-form_societe.php">Ajouter une Société</a></li>
+			<li><a href="bo-gestion_societe.php">Liste des sociétés</a></li>
+			<li><a href="bo-form_societe.php">Ajouter une société</a></li>
 			<li><a href="bo-Licences.php">Licences distribuées</a></li>
 		</ul>
 	</li>
